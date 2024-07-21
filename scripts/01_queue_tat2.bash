faa711e7c4ccd919d2638067b4aa64bb21dad017
Will Foran <foranw@br013.ib.bridges2.psc.edu>
2024-07-21 10:44:35 -0400

    🔊 tat2_visit: log total time; queue_tat2 matching submit/skip echo

--- scripts/01_queue_tat2.bash
+++ scripts/01_queue_tat2.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source $(dirname $0)/../../setup_path.sh
 #
 # submit jobs (tat2_visit.bash) for each visit and rac1+rac2
 # use DRYRUN=1 ./01_queue_tat2.bash
@@ -14,14 +15,14 @@ for rest in 1 2; do
     jobname=tat2-$ld8-$rest
     logfile=log/%x_%A_%j.txt
     final_out="/ocean/projects/soc230004p/shared/datasets/tat2/$ld8/pet$rest/_ref-wholebrain_time-median_vol-median_censor-fd0.3_calc-novol_scale-none_tat2.nii.gz"
-    test -r "$final_out"  && echo "# $jobname: have '$_'; skipping" && continue
-    squeue --me -o %j | grep $jobname && echo "# $jobname in queue, skipping" && continue
-    echo "# $jobname -- submitting"
+    test -r "$final_out"  && echo "# skip $jobname have '$_'" && continue
+    squeue --me -o %j | grep $jobname && echo "# skip $jobname is in queue" && continue
+    echo "# submit $jobname"
     dryrun sbatch \
           -J $jobname \
           -o $logfile -e $logfile \
 	  --export REST_NUM=$rest \
           tat2_visit.bash $ld8
-    #break
+    break
   done
 done
