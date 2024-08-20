53b5bfe62f9230a0271dcbad341ebb0c9d101df0
Will Foran <foranw@br013.ib.bridges2.psc.edu>
2024-08-20 18:23:20 -0400

    🎨02_maskave.bash: rework mean outputs for easier reading

--- scripts/02_maskave.bash
+++ scripts/02_maskave.bash
@@ -19,12 +19,34 @@ fi
 
 roi_atlas=../atlases/HarOx-thr50-CaudPutPalAcc_res-func.nii.gz
 [ ! -r $roi_atlas ] && echo "cannot read roi_atlas: '$roi_atlas'" && exit 1
-tat2_files=(../output/1*_2*/pet*/*_tat2.nii.gz)
-echo "# running for ${#tat2_files[@]} tat2 files"
-mkdir -p ../stats
+
+# use lables to build up command arguments like
+# -m LeftCaudate=atlas.nii.gz<5>
+mapfile -t mask_args < <(
+ cat ../atlases/HarOx-thr50-CaudPutPalAcc_labels.tsv|
+    while read idx roiname; do
+	    # new line so mapfile can separate
+            echo -e "-m\n${roiname/ /}=$roi_atlas<$idx>"
+    done)
+
+
 echo "# nifti $(date)"
-3dROIstats -nzvoxels -nzmean -nomeanout -nzsigma -mask "$roi_atlas" -1DRformat "${tat2_files[@]}" > ../stats/CaudPutPalAcc.tsv
+mkdir -p ../stats
+find ../output/1*_2*/pet*/ -iname '*_tat2.nii.gz' |
+  3dmaskave_grp \
+     -stdinfiles \
+     -roistats -nomeanout,-nzmean,-nzvoxels,-nzsigma \
+     "${mask_args[@]}" \
+     -csv ../stats/CaudPutPalAcc.tsv
+
+# 3dmaskave_grp has nice output, but this is faster
+#3dROIstats -nzvoxels -nzmean -nomeanout -nzsigma -mask "$roi_atlas" -1DRformat "${tat2_files[@]}" > ../stats/roistats
 
 echo "# json info $(date)"
 echo -e 'filename\tvolume_norm_3dROIstats\ttime_norm_3dcalc\tnvox\t.concat_nvol\tnvolx_nocen\texpr' > ../stats/run_info.tsv
-jq -r '[input_filename,.volume_norm_3dROIstats,.time_norm_3dcalc,.nvox[0], .concat_nvol,.nvolx_nocen,.expr]|@tsv' ${tat2_files[@]/.nii.gz/.log.json} >> ../stats/run_info.tsv
+find ../output/1*_2*/pet*/ -iname '*_tat2.log.json' |
+  xargs jq -r \
+    '[input_filename,.volume_norm_3dROIstats,.time_norm_3dcalc,.nvox[0], .concat_nvol,.nvolx_nocen,.expr]|@tsv' \
+    >> ../stats/run_info.tsv
+
+echo "# finished $(date)"
