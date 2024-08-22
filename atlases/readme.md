2e42df4ba369418390144f4018db40562fe86ed6
Will Foran <foranw@br013.ib.bridges2.psc.edu>
2024-08-22 12:14:42 -0400

    ventricle for ref (still unused) and atlas output; R to rearrange after switch back to 3dROIStats

--- atlases/readme.md
+++ atlases/readme.md
@@ -4,6 +4,7 @@ See [Makefile](./Makefile) for `3dcalc`+`3dresample` manipulations of initial vo
 
 |file|neurovalt src|desc|
 |-|-|-|
-|`ref/JHU-ICBM-CCbody_res-func.nii.gz`| [1401](https://neurovault.org/images/1401/) | body of corpus callosum. use as reference to normalize iron concentration|
+|`ref/JHU-ICBM-CCbody_res-func.nii.gz`| [1401](https://neurovault.org/images/1401/) | posterior corpus callosum. use as reference to normalize iron concentration|
+|``|[g-node:CerbraA](https://gin.g-node.org/anamanera/CerebrA/src/master/)|ventricles for refrence norm|
 |`HarOx-thr50-CaudPutPalAcc_res-func.nii.gz`| [1698](https://neurovault.org/images/1698/) | Regions of interest for iron concentration: L & R Caudate, Putamen, Pallidum, Accumbens. See [labels](./HarOx-thr50-CaudPutPalAcc_labels.tsv)|
 
--- scripts/02_maskave.bash
+++ scripts/02_maskave.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --time=0:30:00
+#SBATCH --time=2:30:00
 #SBATCH --partition=RM-shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -17,7 +17,7 @@ else
   cd "$(dirname "$0")"
 fi
 
-roi_atlas=../atlases/HarOx-thr50-CaudPutPalAcc_res-func.nii.gz
+roi_atlas=../atlases/atlas-VentCCCaudPutPalAcc_res-func.nii.gz
 [ ! -r $roi_atlas ] && echo "cannot read roi_atlas: '$roi_atlas'" && exit 1
 
 # use lables to build up command arguments like
@@ -32,19 +32,26 @@ mapfile -t mask_args < <(
 
 echo "# nifti $(date)"
 mkdir -p ../stats
-find ../output/1*_2*/pet*/ -iname '*_tat2.nii.gz' |
-  3dmaskave_grp \
-     -stdinfiles \
-     -roistats -nomeanout,-nzmean,-nzvoxels,-nzsigma \
-     "${mask_args[@]}" \
-     -csv ../stats/CaudPutPalAcc.tsv
+# this is too slow! takes over 2h 30min to run on only half the ROIs!
+# didn't profile, but suspect roistats per file and grep check are bottlenecks
+#find ../output/1*_2*/pet*/ -iname '*_tat2.nii.gz' |
+#  3dmaskave_grp \
+#     -stdinfiles \
+#     -roistats -nomeanout,-nzmean,-nzvoxels,-nzsigma \
+#     -pattern '\d{5}_\d{8}/pet[12]' \
+#     -m ventricles=$roi_atlas'<1>' \
+#     -m CC=$roi_atlas'<2>' \
+#     "${mask_args[@]}" \
+#     -csv ../stats/CaudPutPalAcc.csv
 
 # 3dmaskave_grp has nice output, but this is faster
-#3dROIstats -nzvoxels -nzmean -nomeanout -nzsigma -mask "$roi_atlas" -1DRformat "${tat2_files[@]}" > ../stats/roistats
+
+find ../output/1*_2*/pet*/ -maxdepth 1 -iname '*_tat2.nii.gz' | xargs \
+ 3dROIstats -nzvoxels -nzmean -nomeanout -nzsigma -mask "$roi_atlas" -1DRformat  > ../stats/atlas-roistats.tsv
 
 echo "# json info $(date)"
 echo -e 'filename\tvolume_norm_3dROIstats\ttime_norm_3dcalc\tnvox\t.concat_nvol\tnvolx_nocen\texpr' > ../stats/run_info.tsv
-find ../output/1*_2*/pet*/ -iname '*_tat2.log.json' |
+find ../output/1*_2*/pet*/ -maxdepth 1 -iname '*_tat2.log.json' |
   xargs jq -r \
     '[input_filename,.volume_norm_3dROIstats,.time_norm_3dcalc,.nvox[0], .concat_nvol,.nvolx_nocen,.expr]|@tsv' \
     >> ../stats/run_info.tsv
