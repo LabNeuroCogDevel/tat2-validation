0944d83e9245163fac9210062dd0b907e2ebd74a
WillForan <willforan@gmail.com>
2025-02-19 11:41:34 -0500

    ✨ 03_tsnr_mask_stats.bash: mask in mask counts

--- scripts/mk_tsnr_group_mask/03_tsnr_mask_stats.bash
+++ scripts/mk_tsnr_group_mask/03_tsnr_mask_stats.bash
@@ -10,3 +10,11 @@ for f in tsnr/rac*/1*_2*_thres-3_mask.nii.gz; do
    # remove 3dROIstats header after first time it's printed
    [ -z "$nohead" ] && nohead=1d
 done | tee tsnr/tsnr_idv_stats.tsv
+
+for run in rac1 rac2; do
+   3dROIstats \
+      -1DRformat -nobriklab -nzvoxels -nzvolume \
+      -mask ./tsnr/${run}_thres-3_coverage95bin.nii.gz \
+      tsnr/${run}/1*_2*_thres-3_mask.nii.gz \
+      > tsnr/${run}_maskcnt-idvInGrp.tsv
+done
